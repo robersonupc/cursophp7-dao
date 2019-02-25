eb16b2ad8d832b31b50d3cb9286d371bc3195001
Roberson Santos <robersonupc@gmail.com>
2019-02-24 22:39:33 -0300

    Methedos de lista

--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 
 require_once("config.php");
 
-$usuario = new Usuario();
+//Carrega um usuário
+//$usuario = new Usuario();
+//$usuario->loadById(3);
+//echo $usuario;
 
-$usuario->loadById(3);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("a");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Ana Laura", "123456");
 
 echo $usuario;
 
